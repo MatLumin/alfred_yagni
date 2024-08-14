cc837ab086dc09bfeed7032b8e7abcb0286bb3e8
= <88319573+MatLumin@users.noreply.github.com>
2024-08-14 13:27:43 +0330

    added __dir__

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import flask;
-
+import os;
 
 app = flask.Flask("alfred yagni");
 
+__dir__ = os.path.split(__file__)[0];
 
 def read_file(path:str)->str:
 	with open(path, "r") as f1:
@@ -12,10 +13,10 @@ def read_file(path:str)->str:
 
 @app.route("/main")
 def hh_main():
-	global_style:str = "<style>"+read_file("./static/global_style.css")+"</style>"
-	pre_script:str = read_file("./static/pre_scripts.js");
-	post_scripts:str = read_file("./static/post_scripts.js");
-	main_html:str = read_file("./main.html");
+	global_style:str = "<style>"+read_file(f"{__dir__}/static/global_style.css")+"</style>"
+	pre_script:str = read_file(f"{__dir__}/static/pre_scripts.js");
+	post_scripts:str = read_file(f"{__dir__}/static/post_scripts.js");
+	main_html:str = read_file(f"{__dir__}/main.html");
 	return global_style + pre_script + main_html + post_scripts;
 
 
